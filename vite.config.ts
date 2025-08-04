2fb1e9b043ebb046f4721a805e18430d76a34794
Maxwell Gomes <maxwellgomessilva@gmail.com>
2025-08-04 00:26:03 -0300

    atualizado para conexao local segura

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,22 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+import fs from "fs"; // <--- IMPORTANTE!
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
-    host: "::",
+    host: "0.0.0.0", // ← garante que o iPhone possa acessar pelo IP
     port: 8080,
+    https: {
+      key: fs.readFileSync("./ssl-cert/key.pem"),
+      cert: fs.readFileSync("./ssl-cert/cert.pem"),
+    },
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === 'development' && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
